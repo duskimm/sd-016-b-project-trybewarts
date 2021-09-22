6b3c62897e2039e21a8815240b5180786e2ae828
Samuel Goncalves <samuelmonteiro21@gmail.com>
2021-09-22 18:45:38 -0300

    Conclui requisito 18

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const password = document.getElementById('password');
 const email = document.getElementById('email');
 const headerButton = document.getElementById('header-button');
+const submitBtn = document.getElementById('submit-btn');
+const agreementImput = document.getElementById('agreement');
+let checked = false;
 
 function login() {
   if (email.value === 'tryber@teste.com' && password.value === '123456') {
@@ -9,5 +12,22 @@ function login() {
     alert('Email ou senha inválidos');
   }
 }
+
+
+function concordar() {
+  if (checked === false) {
+    checked = true;
+  } else {
+    checked = false;
+  }
+
+  if (checked) {
+    submitBtn.removeAttribute('disabled');
+  } else {
+    submitBtn.setAttribute('disabled', null);
+  }
+}
+
+agreementImput.addEventListener('click', concordar);
 
 headerButton.addEventListener('click', login);
