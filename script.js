7e6d904165ad7eb5523d157f79881638ed5e77e4
vinisavordelli <vinicius.savordelli@gmail.com>
2021-09-22 20:04:04 -0300

    lint e requisito 19

--- script.js
+++ script.js
@@ -14,11 +14,10 @@ const agreement = document.getElementById('agreement');
 const submitBtn = document.getElementById('submit-btn');
 
 function habilitaBtn() {
-    if (agreement.checked === true) {
-      submitBtn.disabled = false;
-    } else {
-      submitBtn.disabled = true;
-    }
+  if (agreement.checked === true) {
+    submitBtn.disabled = false;
+  } else {
+    submitBtn.disabled = true;
   }
+}
 agreement.addEventListener('change', habilitaBtn);
-
