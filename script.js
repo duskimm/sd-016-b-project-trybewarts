4c2ae3efc2e9240e8bcbf736a6df6c5688d32b90
Gabriel A <gabrielaug4@gmail.com>
2021-09-22 16:22:48 -0300

    requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,12 @@
+const email = document.getElementById('email');
+const senha = document.getElementById('senha');
+const botao = document.getElementById('botao');
+
+function validarEmailSenha() {
+  if(email.value === 'tryber@tester.com' && senha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+botao.addEventListener('click', validarEmailSenha);
