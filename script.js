aef9492c3a7b0f71c4ed297697a6be75245cb9f4
Seu nome <senamatheus.job@gmail.com>
2021-09-22 17:05:18 -0300

    requisitos 14, 15 , 16 ,17 e 19

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-let loginButton = document.querySelector('.loginBtn');
-let emailUser = document.querySelector('.user-email');
-let userPassword = document.querySelector('.user-password')
+const loginButton = document.querySelector('.loginBtn');
+const emailUser = document.querySelector('.user-email');
+const userPassword = document.querySelector('.user-password');
 
-loginButton.addEventListener('click',function(){
-  let isValid = emailUser.value === 'tryber@teste.com' && userPassword.value === '123456' ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.')
+loginButton.addEventListener('click', function () {
+  const isValid = emailUser.value === 'tryber@teste.com' && userPassword.value === '123456' ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.');
 
   return isValid;
-  
-})
+});
