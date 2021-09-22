1fc4f7045bbc4dc767c0a05bea6599ec1fa60a01
morallys <morallys.juan@gmail.com>
2021-09-22 16:35:09 -0300

    Requisitos 3 e 4 check

---
+++ script.js
@@ -0,0 +1,11 @@
+const btnLogin = document.getElementById('btn-login');
+
+btnLogin.addEventListener('click', () => {
+  const email = document.getElementById('input-email').value;
+  const password = document.getElementById('input-password').value;
+
+  if (email !== 'tryber@teste.com' && password !== '123456') {
+    return alert('Email ou senha inválidos.');
+  }
+  return alert('Olá, Tryber!');
+});
