6f7557cb971ca67a4974f2c97b49a64b0de93133
Gilson Alencar <gilson.alencar94@gmail.com>
2021-09-22 16:38:39 -0300

    Requisitos 4-7

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const loginButton = document.getElementById('login');
 
-loginButton.addEventListener('click', function (event) {
+loginButton.addEventListener('click', (event) => {
   event.preventDefault();
   const email = document.getElementById('email').value;
   const senha = document.getElementById('senha').value;
   console.log(senha);
-  if (email === 'tryber@teste.com' && senha === '123456'){
+  if (email === 'tryber@teste.com' && senha === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-})
+});
