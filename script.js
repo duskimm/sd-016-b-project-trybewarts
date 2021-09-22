e7aa01798758639a69d84f5df3f25ddec8b33059
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2021-09-22 19:27:01 -0300

    Requisito 18

--- script.js
+++ script.js
@@ -3,11 +3,21 @@ const passwordTrybe = '123456';
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('password');
 const btnLogin = document.getElementById('btnLogin');
+const checkAgreement = document.getElementById('agreement');
+const btnSend = document.getElementById('submit-btn');
 
 btnLogin.addEventListener('click', () => {
   if (inputEmail.value === emailTrybe && inputPassword.value === passwordTrybe) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
+  }
+});
+
+checkAgreement.addEventListener('change', (event) => {
+  if (event.target.checked) {
+    btnSend.removeAttribute('disabled');
+  } else {
+    btnSend.setAttribute('disabled', 'disabled');
   }
 });
