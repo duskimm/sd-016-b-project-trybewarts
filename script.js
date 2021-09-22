75250b49ce18b9036b0847a898aef3a1fb3cbd04
Erik Duarte Rodrigues <erikduarterodrigues@gmail.com>
2021-09-22 17:32:18 -0300

    Entrega desafio 3

---
+++ script.js
@@ -0,0 +1,15 @@
+const email = document.querySelector('#input-email');
+const senha = document.querySelector('#input-senha');
+const loginBtn = document.querySelector('#login-btn');
+
+function checkEmail() {
+  if (email.value === "tryber@teste.com" && senha.value === "123456") {
+    alert("Olá, Tryber!"); 
+  } else {
+    alert("Email ou senha inválidos.");
+  }
+};
+
+loginBtn.addEventListener('click', checkEmail);
+
+
