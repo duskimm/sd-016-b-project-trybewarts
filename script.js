5facdf808cc438048976ddd775765b10bb2ffa26
vinisavordelli <vinicius.savordelli@gmail.com>
2021-09-22 16:29:01 -0300

    requisitos ate o 8

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 
 loginButton.addEventListener('click', function() {
-    if (email.value == 'tryber@teste.com' && password.value == '123456'){
+    if (email.value === 'tryber@teste.com' && password.value === '123456'){
         alert('Olá, Tryber!');
     } else {
         alert('Email ou senha inválidos.');
