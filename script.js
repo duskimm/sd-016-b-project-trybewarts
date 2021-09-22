a94c47d8c0f80702acfc36a992450707a4b5a47d
Danilo-Couto <couto.danilo95@gmail.com>
2021-09-22 20:01:10 -0300

    requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-let buttonEntrar = 
+/* REQUISITO 3 */
+
+let buttonEntrar = document.getElementById('botaoEntrar');
 let inputLogin = document.getElementById('email');
 let inputPassword = document.getElementById('password');
 
 function login () {
-    if (inputLogin === 'tryber@teste.com' && inputPassword === '123456') {
+    if (inputLogin.value === 'tryber@teste.com' && inputPassword.value == '123456') {
         window.alert ('Olá, Tryber!')
     } else {
         window.alert ('Email ou senha inválidos.')
