8cc6f581bb749f22255f335c401f5b5f082580a3
André Melo <adr.smelo@gmail.com>
2021-09-22 17:05:21 -0300

    C4 requisito 3 finalizado js oky

---
+++ script.js
@@ -0,0 +1,17 @@
+const btnSub = document.getElementById('btn-sub');
+
+function email() {
+  const inputEmail = document.getElementById('input-email').value;
+  const inputSenha = document.getElementById('input-senha').value;
+  if (inputEmail === 'tryber@teste.com' && inputSenha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+}
+}
+
+function loginValidate(e) {
+  e.preventDefault();
+  email();
+}
+btnSub.addEventListener('click', loginValidate);
