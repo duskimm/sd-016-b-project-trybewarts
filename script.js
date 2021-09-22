53f0dc60c51483f055eeded3cacb0c62fdd1448e
Lucas <lb.camargo94@gmail.com>
2021-09-22 18:41:17 -0300

    Item 18 concluido

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const inputEmail = document.getElementById('login-email');
 const inputSenha = document.getElementById('login-senha');
 const buttonEntrar = document.getElementById('btn-entrar');
+const chckAgreement = document.getElementById('agreement');
+const buttonEnviar = document.getElementById('submit-btn');
 
-// Validando email e senha e retorna um alert
+// Função valida email e senha, e retorna um alert
 function menssagem() {
   let valueEmail = inputEmail.value;
   console.log(valueEmail);
@@ -15,3 +17,13 @@ function menssagem() {
   }
 }
 buttonEntrar.addEventListener('click', menssagem);
+
+// Função habilita botão submit
+function submitOn() {
+  if (chckAgreement.checked) {
+    buttonEnviar.removeAttribute('disabled');
+  } else {
+    buttonEnviar.setAttribute('disabled', 'disabled');
+  }
+}
+chckAgreement.addEventListener('change', submitOn);
