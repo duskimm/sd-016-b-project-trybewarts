dfc81576222d1bded873cacc40a84b3b600406b2
Nathalia <nathaliamagi@gmail.com>
2021-09-22 17:40:19 -0300

    Requisito-3

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
-const submitButton = document.getElementById('btn-entrar');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
+const submitButton = document.querySelector('#btn-entrar');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
 
 function validateLog(event) {
   event.preventDefault();
-  console.log(email.value);
-  /*
   if ((email.value === 'tryber@teste.com') && (password.value === '123456')) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
-  */
-
 }
 
 submitButton.addEventListener('click', validateLog);
-
