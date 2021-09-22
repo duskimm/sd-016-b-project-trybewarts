38911ec08c74a3e8e5dbbde91fc795b9f54dd951
Serafim <guilhermeserafimbonfim@hotmail.com>
2021-09-22 18:56:11 -0300

    resolve ate o requisito 18

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const btnSubmit = document.getElementById('btn-submit');
 const inputLogin = document.querySelectorAll('input');
+const agreement = document.getElementById('agreement');
+const sendButton = document.getElementById('submit-btn');
 
 function login() {
   if (inputLogin[0].value === 'tryber@teste.com' && inputLogin[1].value === '123456') {
@@ -9,4 +11,24 @@ function login() {
   }
 }
 
+function disableButton() {
+  if (agreement.checked === false) {
+    sendButton.disabled = true;
+  }
+}
+
+function activeButton() {
+  if (agreement.checked === true) {
+    sendButton.disabled = false;
+  }
+  if (agreement.checked === false) {
+    sendButton.disabled = true;
+  }
+}
+
 btnSubmit.addEventListener('click', login);
+agreement.addEventListener('click', activeButton);
+
+window.onload = function start() {
+  disableButton();
+};
