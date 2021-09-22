57731f9d2761c226a5376cea06fd594d943a8a0a
Bruno Alves Fay <brunofay1@hotmail.com>
2021-09-22 18:20:24 -0300

    14 a 17

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const loginButton = document.getElementById('login-button');
 const login = document.getElementById('email');
 const password = document.getElementById('password');
-
+const checkAgree = document.getElementById('agreement')
 function verifyLogin(){
     if (login.value === 'tryber@teste.com' && password.value === '123456') {
         return alert('Olá, Tryber!');
