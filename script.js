538445f90ae8e724cd1965d481fad294ba24a4b4
Eduardo Fradique <74576664+edsfradique@users.noreply.github.com>
2021-09-22 16:50:13 -0300

    C2 - requisitos 2, 3, 4

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-// const lint = 0;
+const inputEmail = document.getElementById('email');
+const inputPasswd = document.getElementById('senha');
+const botaoLogin = document.querySelector('.button');
+
+function button() {
+  if (inputEmail.value === 'tryber@teste.com' && inputPasswd.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+function chamaButton() {
+  botaoLogin.addEventListener('click', button);
+}
+
+window.onload = function init() {
+  chamaButton();
+};
+
+// Caso o email seja "tryber@teste.com" e a senha seja "123456" será emitido um alerta contendo o texto "Olá, Tryber!"
+// Em todos os outro casos deverá ser emitido um alerta contendo o texto "Email ou senha inválidos."
