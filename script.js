ccaa7eb677b122e6eb68384b9302ea19cefa1be6
Israel Sant'Anna <israel.santanna@gmail.com>
2021-09-22 19:26:54 -0300

    BONUS Requisito 20 - logica do contador

--- script.js
+++ script.js
@@ -39,7 +39,21 @@ function addAgreementEvent() {
   });
 }
 
+function addCommentEvents() {
+  const textarea = document.getElementById('textarea');
+  const counter = document.getElementById('counter');
+  // Lógica para contar, por Etienne Martin, encontrada em:
+  // https://stackoverflow.com/questions/14086398/count-textarea-characters/43837521#43837521
+  textarea.addEventListener('input', (evt) => {
+    const target = evt.currentTarget;
+    const maxLength = target.getAttribute('maxlength');
+    const currentLength = target.value.length;
+    counter.innerText = `${maxLength - currentLength}`;
+  });
+}
+
 window.onload = () => {
   addFormEvents();
   addAgreementEvent();
+  addCommentEvents();
 };
