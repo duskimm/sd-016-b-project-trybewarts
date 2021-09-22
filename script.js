6c654879cfd813d7f15ea95555b614bc471c2006
Seu nome <senamatheus.job@gmail.com>
2021-09-22 18:11:16 -0300

    Finalizando todos os requisitos obrigatórios

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
 const loginButton = document.querySelector('.loginBtn');
 const emailUser = document.querySelector('.user-email');
 const userPassword = document.querySelector('.user-password');
+const agreeBox = document.querySelector('#agreement')
+let subtmitButton = document.querySelector('#submit-btn')
+subtmitButton.disabled = true;
+
+
 
 loginButton.addEventListener('click', function () {
   const isValid = emailUser.value === 'tryber@teste.com' && userPassword.value === '123456' ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.');
 
   return isValid;
 });
+
+agreeBox.addEventListener('click', function(evt){
+
+  if(evt.target.checked === true){
+    subtmitButton.disabled = false
+  } else {
+    subtmitButton.disabled = true
+  }
+
+})
