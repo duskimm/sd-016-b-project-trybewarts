dace8c389cd979504e720766acab1dc2870e30b1
Istvan <olivaistvan@gmail.com>
2021-09-22 16:42:05 -0300

    Requisito 09

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ const loginButton = document.getElementById('buttonLogin');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-loginButton.addEventListener('click', function() {
-    if (email.value === 'tryber@teste.com' && password.value === '123456'){
-        alert('Olá, Tryber!');
-    } else {
-        alert('Email ou senha inválidos.');
-    }
+loginButton.addEventListener('click', function login () {
+  if (email.value === 'tryber@teste.com' && password.value === '123456'){
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 });
