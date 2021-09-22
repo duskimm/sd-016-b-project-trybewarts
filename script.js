1d42767359842ddc4c538fdc1e56355bbf538022
Douglas Ialamov <douglas.ialamov@gmail.com>
2021-09-22 16:50:23 -0300

    Exercícios 12 e 13 feitos

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 let loginButton = document.querySelector('.loginBtn');
 let emailUser = document.querySelector('.user-email');
-let userPassword = document.querySelector('.user-password')
+let userPassword = document.querySelector('.user-password');
 
-loginButton.addEventListener('click',function(){
-  let isValid = emailUser.value === 'tryber@teste.com' && userPassword.value === '123456' ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.')
+loginButton.addEventListener('click', function() {
+  let isValid = emailUser.value === 'tryber@teste.com' && userPassword.value === '123456' ? alert('Olá, Tryber!') : alert('Email ou senha inválidos.');
 
   return isValid;
-  
 })
