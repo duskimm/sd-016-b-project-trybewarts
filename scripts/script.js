c210957956c208c3c8c1fdfefcf5e850df936251
Gabriel Silvestre <gabriel.h.silvestre11@gmail.com>
2021-09-22 15:21:19 -0300

    starting html and js

---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+function addMultiplesEvents(element, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  events.forEach((event) => {
+    element.addEventListener(event, listener, false);
+  });
+}
+
+function addMultiplesListeners(arr, eventName, listener) {
+  arr.forEach((element) => {
+    element.addEventListener(eventName, listener, false);
+  });
+}
+
+function addMultiplesEventsAndListeners(arr, eventsName, listener) {
+  const events = eventsName.split(' ');
+
+  arr.forEach((element) => {
+    events.forEach((event) => {
+      element.addEventListener(event, listener, false);
+    });
+  });
+}
