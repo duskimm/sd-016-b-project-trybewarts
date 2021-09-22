1467d4bda8e9eb10ef82cb73bc6b4706f52db954
bruno Marques <blmarques.dev@gmail.com>
2021-09-22 19:17:30 -0300

    requirement 13 done

--- src/scripts.js
+++ src/scripts.js
@@ -1,2 +1,13 @@
+
+
 const btnEntrar = document.querySelector('button');
-btnEntrar.addEventListener('click', () => { alert('Olá, Tryber!'); });
+btnEntrar.addEventListener('click', () => {
+  const email = document.querySelector('.emailVal').value;
+  const senha = parseInt(document.querySelector('.pswVal').value, 10);
+  const psw = 123456;
+  const mail = 'tryber@teste.com';
+  if (psw === senha && mail === email) {
+    return alert('Olá, Tryber!');
+  }
+  return alert('Email ou senha inválidos.');
+});
