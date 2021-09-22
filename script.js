63fef8c1eeb7c9dd234b30645ffbb8270ca29810
Istvan <olivaistvan@gmail.com>
2021-09-22 17:53:05 -0300

    ajustes de lint

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const loginButton = document.getElementById('buttonLogin');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
 
-loginButton.addEventListener('click', function login(){
-  if (email.value === 'tryber@teste.com' && password.value === '123456'){
+loginButton.addEventListener('click', () => {
+  if (email.value === 'tryber@teste.com' && password.value === '123456') {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
