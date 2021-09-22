1e28585beaafbec47bb771f4c85c90de3e0e2832
Felipe Pugas <felipepcan@gmail.com>
2021-09-22 18:42:11 -0300

    requisitos 17 e 18

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const email = document.getElementById('email');
 const senha = document.getElementById('senha');
 const botao = document.getElementById('botao');
+const sendButton = document.querySelector('#submit-btn');
+const agreementChecked = document.querySelector('#agreement');
 
 function validarEmailSenha() {
   if (email.value === 'tryber@teste.com' && senha.value === '123456') {
@@ -10,3 +12,13 @@ function validarEmailSenha() {
   }
 }
 botao.addEventListener('click', validarEmailSenha);
+
+function radioChecked() {
+  if (agreementChecked.checked) {
+    sendButton.disabled = false;
+  } else {
+    sendButton.disabled = true;
+  }
+}
+
+agreementChecked.addEventListener('click', radioChecked);
