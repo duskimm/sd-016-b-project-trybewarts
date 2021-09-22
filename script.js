333d23c6f3035e51674fdc5b6187d25dc4a2035f
Lucas <lb.camargo94@gmail.com>
2021-09-22 16:28:11 -0300

    Item 3 concluido

---
+++ script.js
@@ -0,0 +1,17 @@
+const inputEmail = document.getElementById('input-email');
+const inputSenha = document.getElementById('input-senha');
+const buttonEntrar = document.getElementById('btn-entrar');
+
+// Validando email e senha e retorna um alert
+function menssagem() {
+  let valueEmail = inputEmail.value;
+  console.log(valueEmail);
+  let valueSenha = inputSenha.value;
+  console.log(valueSenha);
+  if (valueEmail === 'tryber@teste.com' && valueSenha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+      alert('Email ou senha inválidos.');
+  }
+}
+buttonEntrar.addEventListener('click', menssagem);
