9ab093ab40e940f94a4410f6a38216a17699c0cd
Rafa Selistre <rafael_selistre@hotmail.com>
2021-09-22 18:44:15 -0300

    requisitos 3 e 4 finalizados

---
+++ script.js
@@ -0,0 +1,19 @@
+const inputEmail = document.getElementById('email');
+const inputPasswd = document.getElementById('senha');
+const botaoLogin = document.querySelector('.button');
+
+function button() {
+  if (inputEmail.value === 'tryber@teste.com' && inputPasswd.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  };
+};
+
+function chamaButton() {
+  botaoLogin.addEventListener('click', button);
+};
+
+window.onload = function init() {
+  chamaButton();
+};
