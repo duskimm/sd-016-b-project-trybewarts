dfbba0397b1117378a279e41c50dc3411261d559
Istvan <olivaistvan@gmail.com>
2021-09-22 16:12:36 -0300

    Requisito 03

---
+++ script.js
@@ -0,0 +1,11 @@
+const loginButton = document.getElementById('buttonLogin');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+loginButton.addEventListener('click', function() {
+    if (email.value == 'tryber@teste.com' && password.value == '123456'){
+        alert('Olá, Tryber!');
+    } else {
+        alert('Email ou senha inválidos.');
+    }
+});
