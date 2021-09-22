c906ec4bab83770e3db63ad876917934870d60fe
calidor <victor.calidor@gmail.com>
2021-09-22 18:57:26 -0300

    correçoes no lint

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 /// Verificar se o login está correto
-const loginButton = document.getElementById("login-button");
+const loginButton = document.getElementById('login-button');
 
 function verifyLogin() {
-  const password = document.getElementById("password").value;
+  const password = document.getElementById('password').value;
   console.log(password);
-  const email = document.getElementById("email").value;
+  const email = document.getElementById('email').value;
   console.log(email);
-  if (password === "123456" && email === "tryber@teste.com") {
-    alert("Olá, Tryber!");
+  if (password === '123456' && email === 'tryber@teste.com') {
+    alert('Olá, Tryber!');
   } else {
-    alert("Email ou senha inválidos.");
+    alert('Email ou senha inválidos.');
   }
 }
 
