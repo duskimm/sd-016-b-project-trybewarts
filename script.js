967450d20c9e3579213cf8abbe468752cdaf5467
Fredi Wilkens <frediwilkens@gmail.com>
2021-09-23 00:24:04 -0300

    Finalização de projeto Linter ok

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const lastNameInput = document.getElementById('input-lastname');
 const emailInput = document.getElementById('input-email');
 const houseInput = document.getElementById('house');
 const form = document.getElementById('evaluation-form');
-
+const space = ' ';
 
 btnLogin.addEventListener('click', () => {
   const email = document.getElementById('input-login').value;
@@ -34,31 +34,34 @@ textArea.addEventListener('input', () => {
   counter.innerText = total - caracteres;
 });
 
+/* Utilizamos este site de inspiração: https://pt.stackoverflow.com/questions/83463/pegar-valores-checkbox-com-javascript para a função abaixo */
+
 function getLearningValue() {
   const learning = document.querySelectorAll('[name=learning]:checked');
   const value = [];
   for (let index = 0; index < learning.length; index += 1) {
-    if (index === 0){
+    if (index === 0) {
       value.push(learning[index].value);
     } else {
-      value.push(' ' + learning[index].value);
+      value.push(space + learning[index].value);
     }
   }
   return value;
 }
 
 function createElements() {
+  const joker = ': ';
   const elementos = ['Nome', 'Email', 'Casa', 'Família', 'Matérias', 'Avaliação', 'Observações'];
   for (let index = 0; index < elementos.length; index += 1) {
     const paragraph = document.createElement('p');
-    paragraph.innerText = elementos[index] + ': ' + localStorage.getItem(elementos[index]);
+    paragraph.innerText = elementos[index] + joker + localStorage.getItem(elementos[index]);
     form.appendChild(paragraph);
   }
 }
 
 window.onload = () => {
   btnSubmit.addEventListener('click', () => {
-    localStorage.setItem('Nome', nameInput.value + ' ' + lastNameInput.value);
+    localStorage.setItem('Nome', nameInput.value + space + lastNameInput.value);
     localStorage.setItem('Email', emailInput.value);
     localStorage.setItem('Casa', houseInput.value);
     localStorage.setItem('Família', document.querySelector('[name=family]:checked').value);
@@ -69,4 +72,4 @@ window.onload = () => {
     createElements();
     localStorage.clear();
   });
-} 
+};
