8f048a0e47a539b13cc5c1f9c4469108528e20f3
MychaelJales <mychael_duarte@hotmail.com>
2021-09-22 21:06:07 -0300

    Resolvendo os requisitos 1, 2 e 3

---
+++ script.js
@@ -0,0 +1,18 @@
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
+function validateLogin(event) {
+  event.preventDefault();
+  const emailCorrect = 'tryber@teste.com';
+  const passwordCorrect = '123456';
+  if ((email.value === emailCorrect) && (password.value === passwordCorrect)) {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+window.onload = function () {
+  const btnLogin = document.getElementById('login');
+  btnLogin.addEventListener('click', validateLogin);
+};
