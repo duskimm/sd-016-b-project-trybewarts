72d41b28b498326a251680b313f3efeefb3df4ea
Orlando Dantas <orlandodantas.1@hotmail.com>
2021-09-22 22:23:08 -0300

    requisitos tres e quatro concluidos

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
-let inputEmail = document.getElementById('#inputEmail');
-let inputSenha = document.getElementById('#inputSenha');
+const inputEmail = document.getElementById('inputEmail');
+const inputSenha = document.getElementById('inputSenha');
 
-function button() {
-  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+function button(evento) {
+  evento.preventDefault();
+  if ((inputEmail.value === 'tryber@teste.com') && (inputSenha.value === '123456')) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
-  };
-};
+  }
+}
 
-let buto = document.querySelector('.button');
+const botaoLogin = document.querySelector('.button');
 
 function chamaButton() {
   botaoLogin.addEventListener('click', button);
-};
+}
 
-window.onload = function init() {
+window.onload = () => {
   chamaButton();
 };
