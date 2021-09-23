64ee6a400e87822f378adb99f079611a6eec2b32
morallys <morallys.juan@gmail.com>
2021-09-23 04:27:02 -0300

    Correções de lint, finalização do projeto

--- script.js
+++ script.js
@@ -73,12 +73,3 @@ window.onload = () => {
     localStorage.clear();
   });
 };
-
-function myFunction() {
-  const x = document.getElementById('myLinks');
-  if (x.style.display === 'block') {
-    x.style.display = 'none';
-  } else {
-    x.style.display = 'block';
-  }
-}
