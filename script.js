f8ad67de5ce3f60e4178e07e15e9883502f117f1
Nayara Vasconcelos <nayaravasconcelos.dev@gmail.com>
2021-09-22 23:04:29 -0300

    Ajusta head e forms no HTML e adiciona window.onload no script.

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// requisito 3 validação login e senha
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 const loginBtn = document.querySelector('#login-btn');
+const agreement = document.getElementById('agreement');
+const submitBtn = document.getElementById('submit-btn');
 
-function validateLogin(event) {
-  event.preventDefault();
+function validateLogin() {
+  // Parte do requisito 3: Confere se o login é válido e alerta.
   if ((email.value === 'tryber@teste.com') && (password.value === '123456')) {
     alert('Olá, Tryber!');
   } else {
     alert('Email ou senha inválidos.');
   }
 }
-loginBtn.addEventListener('click', validateLogin);
 
-// requisito 18 adicionar evento disable
-const agreement = document.getElementById('agreement');
-const submitBtn = document.getElementById('submit-btn');
-
-function checkedAgreement() {
+function enableSubmitBtn() {
+  // Requisito 18: Confere se o atributo 'disable' está configurado no botão de enviar o formulário.
+  // Se true, remove o atributo. Se false, adiciona o atributo.
   submitBtn.toggleAttribute('disabled');
 }
-agreement.addEventListener('change', checkedAgreement);
+
+window.onload = function trybewartsProject() {
+  loginBtn.addEventListener('click', validateLogin); // Chama a função validateLogin ao clicar no botão do login.
+  agreement.addEventListener('change', enableSubmitBtn); // Chama a função enableSubmitBtn quando o checkbox é marcado ou desmarcado.
+};
