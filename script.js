5fed33253285986b95f65a0807b288bcaa370339
Victor Shin <victor.kamiguchi@gmail.com>
2021-09-22 22:34:51 -0300

    C21 - Requisito Bonus 21 - Finalizado - TODOS REQUISITO COMPLETADOS

--- script.js
+++ script.js
@@ -4,17 +4,42 @@ const ageChckBox2 = document.getElementById('agreement');
 const textArea = document.getElementById('textarea');
 const paragraph = document.getElementById('counter');
 
-paragraph.innerText = 500;
+function initialSet() {
+  paragraph.innerText = 500;
+  btnSub2.disabled = true;
+}
+
+function mater() {
+  const materias = document.getElementsByClassName('subject');
+  const matCheck = [];
+  for (let i = 0; i < materias.length; i += 1) {
+    const matAtual = materias[i];
+    if (matAtual.checked) {
+      matCheck.push(matAtual.value);
+    }
+  }
+  return matCheck.join(', ');
+}
+
+function subForm(e) {
+  e.preventDefault();
+  const nameInput = document.querySelector('#input-name').value;
+  const lastNInput = document.querySelector('#input-lastname').value;
+  const eInput = document.querySelector('#input-email').value;
+  const houseInput = document.querySelector('#house').value;
+  const formTxt = document.querySelector('#form-txt');
+  const aval = document.querySelector('input[name="rate"]:checked').value;
+  const familia = document.querySelector('input[name="family"]:checked').value;
+  const obs = document.querySelector('#textarea').value;
+  formTxt.innerText = `Nome: ${nameInput} ${lastNInput} Email: ${eInput} Casa: ${houseInput}
+  Família: ${familia} Matérias: ${mater()} Avaliação: ${aval} Observações: ${obs}`;
+}
 
 function countChar() {
   const numChar = textArea.value.length;
   const count = 500 - numChar;
   paragraph.innerText = count;
-  // paragraph.innerText = `Caracteres restantes: ${count}`;
 }
-textArea.addEventListener('keyup', countChar);
-
-btnSub2.disabled = true;
 
 function disable(e) {
   const eTar = e.target.checked;
@@ -40,5 +65,8 @@ function loginValidate(e) {
   email();
 }
 
+window.onload = initialSet;
+textArea.addEventListener('keyup', countChar);
 ageChckBox2.addEventListener('change', disable);
 btnSub.addEventListener('click', loginValidate);
+btnSub2.addEventListener('click', subForm);
