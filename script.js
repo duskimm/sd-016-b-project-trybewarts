5d1cdb1ca117c251bf74ceb77b012321a01a4f1d
morallys <morallys.juan@gmail.com>
2021-09-23 04:16:09 -0300

    Adicionando arquivo final do projeto Trybewarts

--- script.js
+++ script.js
@@ -73,3 +73,12 @@ window.onload = () => {
     localStorage.clear();
   });
 };
+
+function myFunction() {
+  const x = document.getElementById('myLinks');
+  if (x.style.display === 'block') {
+    x.style.display = 'none';
+  } else {
+    x.style.display = 'block';
+  }
+}
