4c399fc83269765965d37717bcfa0aa499e4e9d9
HugoDaniel2000 <hugodanielc67@gmail.com>
2021-09-22 23:29:30 -0300

    requisito 18, requisito 20

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 const inputEmail = document.getElementById('email');
 const inputPassword = document.getElementById('senha');
 const buttonEnter = document.getElementById('btn-entrar');
+const checkAgreement = document.getElementById('agreement');
+const btnSubmit = document.getElementById('submit-btn');
+const textArea = document.getElementById('textarea');
+const countCaracters = document.getElementById('counter');
+// const inputName = document.getElementById('input-name');
+// const inputLastName = document.getElementById('input-lastname');
+// const inputEmailForm = document.getElementById('input-email');
+// const house = document.getElementById('house');
+// const level = document.getElementsByName('rate');
 
 buttonEnter.addEventListener('click', (event) => {
   event.preventDefault();
@@ -10,3 +19,35 @@ buttonEnter.addEventListener('click', (event) => {
     alert('Email ou senha inválidos.');
   }
 });
+
+function agreementValidation() {
+  if (checkAgreement.checked) {
+    btnSubmit.disabled = false;
+  } else {
+    btnSubmit.disabled = true;
+  }
+}
+agreementValidation();
+
+checkAgreement.addEventListener('click', agreementValidation);
+// let count = 500;
+// function textCount() {
+//   count -= textArea.value.length;
+//   countCaracters.innerText = `${count}`;
+// }
+
+// textArea.addEventListener('keyup', textCount);
+
+countCaracters.innerText = 'Você ainda tem 500 caracteres!';
+textArea.addEventListener('keyup', () => {
+  const sum = 500 - textArea.value.length;
+  countCaracters.innerText = `Você ainda tem ${sum} caracteres`;
+});
+
+// const data = document.getElementById('data');
+
+// function dataForm() {
+//   data.innerText = `Nome: ${inputName.value} ${inputLastName.value} Email: ${inputEmailForm.value} Observações:${textArea.value} Casa: ${house.value} Avaliação: ${level.value} `;
+// }
+
+// btnSubmit.addEventListener('click', dataForm);
