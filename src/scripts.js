6f1ba6a6b6f4b53e02487d43919b87fd83405e5a
bruno Marques <blmarques.dev@gmail.com>
2021-09-23 01:38:40 -0300

    First requirement done. :rocket:

--- src/scripts.js
+++ src/scripts.js
@@ -1,11 +1,22 @@
-const btnEntrar = document.querySelector('button');
+function query(params) {
+  const term = params;
+  return document.querySelector(term);
+}
+
+const btnEntrar = query('#btnEntrar');
 btnEntrar.addEventListener('click', () => {
-  const email = document.querySelector('.emailVal').value;
-  const senha = parseInt(document.querySelector('.pswVal').value, 10);
+  const email = query('.emailVal').value;
+  const senha = parseInt(query('.pswVal').value, 10);
   const psw = 123456;
   const mail = 'tryber@teste.com';
   if (psw === senha && mail === email) {
     return alert('Olá, Tryber!');
   }
   return alert('Email ou senha inválidos.');
+});
+
+const contractCheck = query('.contractCheck');
+contractCheck.addEventListener('click', () => {
+  const btnSend = query('#submit-btn');
+  btnSend.removeAttribute('disabled');
 });
